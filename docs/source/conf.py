70ea90292cd859cabe7f798851b3b0dc781ab5a8
Sébastien <Tzaphkiel@users.noreply.github.com>
2020-04-26 10:49:42 +0200

    Documented extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,10 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# 
+# Plantuml install for readthedocs: 
+# https://sphinxcontrib-needs.readthedocs.io/en/latest/installation.html#install-plantuml
+#
 extensions = [
     'sphinx_rtd_theme',
     'sphinxcontrib.plantuml',
